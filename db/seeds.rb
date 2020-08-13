80660e2a73011f4ab3d1ec917e7dc828d781dbb3
Johann Groll <groll.johann@gmail.com>
2020-08-13 16:56:46 +0200

    ,

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "destroy old seeds"
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+json_serialized = open(url).read
+json = JSON.parse(json_serialized)
+
+puts "starting seeding"
+json["drinks"].each do |ingredient|
+  name = ingredient["strIngredient1"]
+  Ingredient.create(name: name)
+end
+
+Cocktail.create(name: "London Mule")
+Cocktail.create(name: "Amaretto Sour")
+Cocktail.create(name: "Gin Tonic")
+Cocktail.create(name: "Bloody Mary")
+Cocktail.create(name: "Tequila Sunrise")
+puts "seeding finished"
